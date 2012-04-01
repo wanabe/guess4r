63cb8a02b88c8ea58d0afdfb3a00814d4ee4a6f9
wanabe <s.wanabe@gmail.com>
2012-04-01 09:48:06 +0900

    use test/unit.

--- test.rb
+++ test.rb
@@ -1,13 +1,29 @@
 ﻿# coding:utf-8
+require "test/unit"
 require_relative "guess"
 
-ary = ["sjis", "utf-8", "euc-jp"].map do |coding|
-  "本日は晴天なり".encode(coding).force_encoding(Encoding::ASCII_8BIT)
-end
+class TestGuess < Test::Unit::TestCase
+  def setup
+    test_string = "本日は晴天なり"
+    @codings = ["sjis", "utf-8", "euc-jp"].map do |name|
+      Encoding.find(name)
+    end
+    @strings = @codings.map do |coding|
+      test_string.encode(coding).force_encoding(Encoding::ASCII_8BIT)
+    end
+  end
 
-ary.each do |str|
-  p str.guess.encoding
-  p str.encoding
-  str.guess!
-  p str.encoding
+  def test_string_guess_encoding
+    assert_equal(@codings, @strings.map {|str| str.guess_encoding})
+  end
+
+  def test_string_guess
+    guessed_table = @strings.map do |str|
+      str.dup.guess!
+    end
+
+    assert_equal(@codings, @strings.map {|str| str.guess.encoding})
+    assert_equal(@codings, guessed_table.map {|str| str.encoding})
+    assert(@strings.all? {|str| str.encoding == Encoding::ASCII_8BIT})
+  end
 end
